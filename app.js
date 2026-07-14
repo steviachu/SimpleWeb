47fcf67c0ef7f518d820a9ce98129ab984a9ef14
steviachu <steviachu@gmail.com>
2026-07-14 22:27:12 +0700

    tambah komentar di index.js dan app.js

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 const d = 122434;
+const e = 1234567;
+// checking if the value of e is equal to 1234567
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const a = 1234567;
 const b = 1234567;
+const c = "yeah berhasil";
+// yeah berhasil
